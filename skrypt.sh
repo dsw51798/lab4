e9e188c85b8f980c6c79d83c7aa92f4c498fbb05
DSW51798 <dsw51798@student.dsw.edu.pl>
2024-06-09 19:46:16 +0200

    Dodano argument --logs

--- skrypt.sh
+++ skrypt.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 if [ "$1" == "--date" ]; then
 	date
+elif [ "$1" == "--logs" ]; then
+	log_amount=100
+	if [ ! -z "$2" ]; then
+		log_amount=$2
+	fi
+	for i in $(seq 1 $log_amount); do
+		filename="log$i.txt"
+		echo "Nazwa pliku: $filename" > $filename
+		echo "Skrypt: $0" >> $filename
+		echo "Data: $(date)" >> $filename
+	done
 fi
